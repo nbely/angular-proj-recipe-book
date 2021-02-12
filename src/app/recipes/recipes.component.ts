f03c75be698a96b776ffd65595c2d7c05b362c4c
Chronicler <chronicler0781@gmail.com>
2021-02-11 20:05:12 -0500

    Update Cross-Component Communication EventEmitters to Subjects

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from './recipe.model'
 import { RecipeService } from './recipe.service';
-
 
 @Component({
   selector: 'app-recipes',
@@ -11,15 +9,9 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    )
   }
 }
