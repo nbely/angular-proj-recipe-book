e904ef07962a872c553c8b28cd8ced41b70d0d69
Chronicler <chronicler0781@gmail.com>
2021-07-09 10:08:35 -0400

    Split App Module into several Feature Modules, added a Shared Module and Core Module for housing shared components and services as well.

---
+++ Sec22_Angular-Modules-&-Optimizing-Angular-Apps/326_Understanding-Shared-Modules/Ending-Files/recipes/recipe.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
+
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Tasty Salad',
+    //         'A tasty and healthy dinner, all in one!',
+    //         'https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_960_720.jpg',
+    //         [
+    //             new Ingredient('Lettuce', 1),
+    //             new Ingredient('Lime Slices', 2),
+    //             new Ingredient('Avocado', 1),
+    //         ]),
+    //     new Recipe(
+    //         'Yummy Chicken Parmesan',
+    //         'Comfort food for all, a classic kitchen staple!',
+    //         'https://c.pxhere.com/images/15/3d/9ee477ee62341b9480ce314b02f8-1417897.jpg!d',
+    //         [
+    //             new Ingredient('Chicken', 5),
+    //             new Ingredient('Pasta Sauce', 1),
+    //             new Ingredient('Mozzarella', 5),
+    //         ])
+    //   ];
+    private recipes: Recipe[] = [];
+
+    constructor(private slService: ShoppingListService) {}
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+}
